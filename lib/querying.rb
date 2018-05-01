77c5efe437a089e5847aab87d0329d42acebba27
Victoria Meng <nights1ststar@protonmail.com>
2018-05-01 20:49:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,6 +34,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON characters.series_id = series.id
   WHERE species = 'human'
+  GROUP BY species
   ORDER BY COUNT(species)"
 end
 
